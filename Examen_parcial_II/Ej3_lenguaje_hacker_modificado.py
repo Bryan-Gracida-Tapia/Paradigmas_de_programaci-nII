9cb8e519fad7b106ccb1f896f56cf9da3b7d50f3
Bryan Gracida Tapia <bryangtapia@gmail.com>
2024-12-14 13:05:41 -0600

    -_-

---
+++ Examen_parcial_II/Ej3_lenguaje_hacker_modificado.py
@@ -0,0 +1,64 @@
+# Gracida Tapia Bryan.
+# 08 de Diciembre del 2024.
+# Descripción:
+# Este programa remplaza caracteres de un texto ingresado dependiendo el lenjuage seleccionado.
+import random
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Función menú.
+def menu():
+    print("** Menú **")
+    print("1) Convertir un texto a lenguaje básico.")
+    print("2) Convertir un texto a lenguaje intermedio.")
+    print("3) Salir.")
+    opcion = int(input("Ingresa una opción: "))
+    return opcion
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Función para convertir el texto dependiendo del nivel elejido.
+def convertir_texto(texto_ingresado,diccionario_basico,diccionario_intermedio,diccionario_digitos):
+    texto_convertido = ""
+    texto_auxiliar = ""
+    if opcion == 1: # ................................ Convertir a lenguaje básico.
+        for caracter in texto_ingresado:
+            texto_auxiliar += diccionario_digitos.get(caracter,caracter)  # Se remplaza el digito si este existe en el diccionario.
+        for caracter in texto_auxiliar:
+            texto_convertido += diccionario_basico.get(caracter, caracter)      # Se remplaza el carácter si este existe en el diccionario.
+
+    else: # ................................ Convertir a lenguaje intermedio.
+        for caracter in texto_ingresado:
+            texto_convertido += diccionario_intermedio.get(caracter,caracter)  # Se remplaza el carácter si este existe en el diccionario.
+
+    return texto_convertido
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Función selección.
+def seleccion(opcion):
+    if opcion == 1 or opcion == 2:
+        texto_ingresado = input("Ingresa texto a convertir: ")
+        texto_covertido = convertir_texto(texto_ingresado, diccionario_basico,diccionario_intermedio,diccionario_digitos)
+    elif opcion == 3:
+        print("Saliendo...")
+    else:
+        print("Opción no válida :(")
+    return texto_covertido
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Código a nivel de módulo.
+# ................................ Diccionarios.
+# Diccionario del nivel básico
+diccionario_basico = {'a': '4','A': '4','e': '3','E': '3','i': '1','o': '0','O': '0','u': '(_)','U': '(_)'}
+# Diccionario del nivel intermedio
+diccionario_intermedio = {'a': '4','A': '4','b': 'l3','B': 'l3','c': '[','C': '[','d': ')','D': ')','e': '3','E': '3','f': '|=','F': '|=','g': '&','G': '&','h': '#','H': '#','i': '1','I': '1','j': ',_|','J': ',_|',
+                                           'k': '>|','K': '>|','l': '1','L': '1','m': '/\/\ ','M': '/\/\ ','n': '^/','N': '^/','o': '0','O': '0','p': '|*','P': '|*','q': '(_,)','Q': '(_,)','r': 'l2','R': 'l2','s': '5','S': '5',
+                                           't': '7','T': '7','u': '(_)','U': '(_)','v': '\/','V': '\/','w': '\/\/','W': '\/\/','x': '><','X': '><','y': 'j','Y': 'j','z': '2','Z': '2','0': 'o','1': 'l','2': 'R','3': 'E','4': 'A','5': 'S','6': 'b','7': 'T','8': 'B','9': 'g'}
+# Diccionario de digitos
+diccionario_digitos = {'0': 'o','1': 'l','2': 'R','3': 'E','4': 'A','5': 'S','6': 'b','7': 'T','8': 'B','9': 'g'}
+
+print("***  Lenguaje hacker (l33t sp34k).  ***")
+opcion = 0
+while opcion != 3:
+    opcion = menu()
+    texto_convertido = seleccion(opcion)
+    print(f"Texto convertido : {texto_convertido}")
+
+    print()
+    print("-----------------------------")
+    print()
---
+++ Examen_parcial_II/Ej4_sombrero_seleccionador_modificado.py
@@ -0,0 +1,83 @@
+# Gracida Tapia Bryan.
+# 08 de Diciembre del 2024.
+# Descripción:
+# Este programa determina la casa (Gryffindor, Slytherin, Hufflepuff y Ravenclaw) a la que se pertenece, de acuerdo a las respuestas ingresadas.
+import random
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Función menú.
+def menu():
+    print("** Menú **")
+    print("[1] Iniciar test")
+    print("[2] Salir")
+    opcion_elejida = int(input("Ingresa una opcion: "))
+    return opcion_elejida
+# ///////////////////////////////////////////////////////////////////////////////////////// Función iniciar el test.
+def iniciar_test():
+    # ................................ Diccionarios.
+    # Diccionario de preguntas
+    preguntas = [{"pregunta": "¿Cuál de las siguientes opciones odiarías más que la gente te llamara?","respuestas": {"Gryffindor": "Ordinario","Slytherin": "Ignorante","Hufflepuff": "Cobarde","Ravenclaw": "Egoísta"}},
+                {"pregunta": "Después de tu muerte, ¿qué es lo que más le gustaría que hiciera la gente cuando escuche tu nombre?","respuestas": {"Gryffindor": "Te extraña, pero sonríe","Slytherin": "Pide más historias sobre tus aventuras","Hufflepuff": "Piensa con admiración tus logros","Ravenclaw": "No me importa lo que piensen de mí después de mi muerte, lo que piensen de mí ahora es lo que cuenta"}},
+                {"pregunta": "Dada la opción, ¿preferirías inventar una poción que garantizara?","respuestas": {"Gryffindor": "Gloria","Slytherin": "Sabiduría","Hufflepuff": "Amor","Ravenclaw": "Poder"}},
+                {"pregunta": "¿Cómo te definirías en una sola palabra?","respuestas": {"Gryffindor": "Valiente","Slytherin": "Ambicioso","Hufflepuff": "Leal","Ravenclaw": "Curioso"}},
+                {"pregunta": "¿Qué cualidad te describe mejor?","respuestas": {"Gryffindor": "La fuerza","Slytherin": "La astucia","Hufflepuff": "La paciencia","Ravenclaw": "La inteligencia"}},
+                {"pregunta": "¿Cuál es tu clase favorita?","respuestas": {"Gryffindor": "Vuelo","Slytherin": "Defensa contra las artes oscuras","Hufflepuff": "Animales fantásticos","Ravenclaw": "Pociones"}},
+                {"pregunta": "¿Cuál es tu lenguaje de programación favorito?","respuestas": {"Gryffindor": "C","Slytherin": "Python","Hufflepuff": "C++","Ravenclaw": "JavaScript"}},
+                {"pregunta": "¿Cuál de los siguientes superpoderes prefieres tener?","respuestas": {"Gryffindor": "Invisibilidad", "Slytherin": "Dormir", "Hufflepuff": "Superfuerza","Ravenclaw": "Teletransportarse"}}
+    ]
+
+    # Diccionario de puntos por casa.
+    puntuaciones = {"Gryffindor": 0, "Slytherin": 0, "Hufflepuff": 0, "Ravenclaw": 0}
+
+    # ................................ Selección aleatoria de 5 preguntas de las 7 existentes en el dicionario de preguntas.
+
+    preguntas_elejidas = random.sample(preguntas, 5)       # la palabra reservada random.sample() selecciona preguntas aleatorias en este caso las ingresadas en el diccionario preguntas, se puede utlizar con conjuntos y listas.
+
+    # ................................ Mostrar las preguntas elijidas y sus opciones.
+
+    for pregunta in preguntas_elejidas:
+        print(f"\n{pregunta['pregunta']}")
+        respuestas = list(pregunta["respuestas"].items())
+        random.shuffle(respuestas)          # la palabra reservada ramdom.shuffle ordena aleatoriamente las opciones.
+        opciones = {str(i + 1): respuesta[1] for i, respuesta in enumerate(respuestas)}
+        for i, opcion in opciones.items():
+            print(f"{i}) {opcion}")
+
+        # ................................ Se lee la respuesta y se asigna el punto a la casa correspondiente en caso de ser valida.
+
+        while True:
+            respuesta = input("Selecciona una opción: ").strip()
+            if respuesta in opciones:
+                # Se busca la casa correspondiente a la respuesta
+                for casa, texto in pregunta["respuestas"].items():
+                    if opciones[respuesta] == texto:       # Se itera en cada una de las de las opciones, y se comparan las respuestas.
+                        puntuaciones[casa] += 1
+                break
+            else:
+                print("Opción no válida. Intenta nuevamente.")
+    # ................................ Se buca la casa con mayor puntuación.
+    # Determinar la casa con mayor puntuación
+    casa_seleccionada = max(puntuaciones, key=puntuaciones.get)     # # la palabra reservada max se utiliza para determinar la puntuación mas alta.
+    print(f"\nEl sombrero seleccionador te ha asignado a la casa {casa_seleccionada}. \n¡Felicidades!")
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Función selección.
+def seleccion(opcion):
+    if opcion == 1:
+        iniciar_test()
+    elif opcion == 2:
+        print("Saliendo...")
+
+    else:
+        print("Opción no válida. Intenta nuevamente.")
+
+# ///////////////////////////////////////////////////////////////////////////////////////// Código a nivel de módulo.
+print("***  Test del sombrero seleccionador de Harry Potter.  ***")
+opcion = 0
+while opcion != 2:
+    opcion = menu()
+    seleccion(opcion)
+
+    print()
+    print("-----------------------------")
+    print()
+
